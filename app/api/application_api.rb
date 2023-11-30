c3bb5347abcc2b4b7e92ff7d3e1b2ac0db9fe0e7
anchueh <an.chu@employmenthero.com>
2023-12-01 03:25:11 +0700

    init empty

--- app/api/application_api.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class ApplicationAPI < Grape::API
-end
--- app/models/user_thread.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class UserThread < ApplicationRecord
-  validates :thread_id, presence: true, uniqueness: true
-  validates :user_id, presence: true, uniqueness: true
-end
